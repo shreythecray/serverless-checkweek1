7dcebc4a21ef13cc3c68ea1587d00b4a5706ca27
Shreya <shreyagopu@gmail.com>
2021-05-23 15:13:05 -0500

    Initial commit

---
+++ .bit/tests/test.1.6.js
@@ -0,0 +1,18 @@
+uri = process.env.TWOCATZ_ENDPOINT
+if (uri[0] != "h") {
+   throw new Error("You have not added your function url as a secret!");
+}
+
+const fs = require('fs') //get the methods in the fs package
+
+//if you wanna add more files, just put a comma after the filename (array)
+const commit_file = ['./../../TwoCatz/index.js']
+
+for(var i = 0; i < commit_file.length; i++) {
+    var a = commit_file[i];
+    fs.access(commit_file[i], err => {
+        if (err) {
+          throw new Error("You did not commit '" + a + "'")
+        }
+    })
+}
